4c1ffbddf478b1f97f8f06bea712f1c89f8c484a
imgitto <68766330+imgitto@users.noreply.github.com>
2021-03-27 13:26:50 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const ytdl = require("ytdl-core");
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+
+app.get("/",function(request,response){
+	response.sendFile(__dirname + "public/index.html");
+});
+
+app.get("/videoInfo",async function(request,response){
+	const videoURL = request.query.videoURL;
+	const info = await ytdl.getInfo(videoURL);
+	response.status(200).json(info);
+});
+
+app.get("/download",function(request,response){
+	const videoURL = request.query.videoURL;
+	const itag = request.query.itag;
+	response.header("Content-Disposition",'attachment;\ filename="video.mp4"');
+	ytdl(videoURL,{
+		filter: format => format.itag == itag
+	}).pipe(response);
+});
+
+app.listen(5000);
